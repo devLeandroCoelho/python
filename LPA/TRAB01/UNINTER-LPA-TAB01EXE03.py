d8b19f77700dd5561d74d165179dbfe30713670c
devLeandroCoelho <devleandrocoelho@gmail.com>
2022-04-21 23:09:45 -0300

    Subindo arquivo 3

--- LPA/TRAB01/UNINTER-LPA-TAB01EXE03.py
+++ LPA/TRAB01/UNINTER-LPA-TAB01EXE03.py
@@ -1,8 +1,36 @@
-altura = "1,00"
-def showMenu(nomeMenu):
-    if nomeMenu == "dim":
-        while (altura.isdigit()):
-            altura = input('Digite a altura do produto (em cm)')
-        comprimento = float(input('Digite o comprimento do produto (em cm)'))
+from pickle import FALSE, TRUE
+altura = 0.00
+a = TRUE
+multi = 0.0
+volume = 0.0
+def dimensoesObejto():
+    try:
+        altura = float(input('Digite a altura do produto (em cm)'))
+        comprimento = float(input('Digite o comprimeto do produto (em cm)'))
         largura = float(input('Digite a largura do produto (em cm)'))
-showMenu('dim')
+        volume = altura * comprimento * largura
+        return volume
+    except ValueError: 
+        print('ERRO!!! Digite um valor númérico!')
+        dimensoesObejto()
+def pesoObejto():
+    try:
+        peso = float(input('Digite o peso do produto (em kg)'))
+        return peso
+        if peso < 0.1: 
+            multi = 1            
+        elif 0.1 < peso <= 1: 
+            multi = 1.5
+        elif 1 < peso <= 10: 
+            multi = 2
+        elif 10 < peso <= 30: 
+            multi = 1.5
+        elif peso > 30: 
+            print('Permitido apenas peso até 30 kg, preencha novamente')
+            pesoObejto()
+    except ValueError: 
+        print('ERRO!!! Digite um valor númérico!')
+        pesoObejto()
+print(volume)
+dimensoesObejto()
+pesoObejto()
