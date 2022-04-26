caac07c8b9756f6707b96751bf0c4284c39d02f0
Leandro Prazeres Coelho <leandro.coelho@stz.com.br>
2022-04-25 23:23:45 -0300

    Ajudando um amigo a fazer uma surpresa!

--- LPA/cpm22.py
+++ LPA/cpm22.py
@@ -1,3 +1,5 @@
+import os
+os.system('clear') or None
 esc = 'cpm22'
 esc2 = 'cpm 22'
 art = 'oi'
@@ -5,20 +7,25 @@ cont = 9
 while (art != 'desisto'):
     art = str(input('Qual o show você gostaria de assistir ou ter assistido?'))
     if (art == esc) or (art ==esc2):
+        os.system('clear') or None
         print('Parabéns, você acertou! Esse é o show que iremos assistir!')
         break
     elif (art == 'dica') or (cont == 0):
-        print('DICA BONUS!!!')
+        os.system('clear') or None
+        print('DICA BONUS!!!\n\n')
         print('"Dias atrás" ou foi "Ontem"')
         print('... tinhamos apenas "1 minuto para o fim do mundo"')
-        print('para assistir um show em uma "Tarde de Outubro" !')
+        print('para assistir um show em uma "Tarde de Outubro" !\n')
         cont = 1
         continue
-    elif (art == 'desisto') or (cont == 5):
-        print('Não desista, você pode pedir uma dica!')
+    elif (art == 'desisto') or (cont <= 5):
+        os.system('clear') or None
+        print('Não desista, você pode pedir uma dica!\n')
+        cont = cont -1
         continue
     else:
-        print('Não é esse o show que iremos ver... Tente novamente!!!')
+        os.system('clear') or None
+        print('Não é esse o show que iremos ver... Tente novamente!!!\n')
         cont = cont -1
         continue
 
