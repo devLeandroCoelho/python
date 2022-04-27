300297504321eb08e650aa64f6224a277e643492
Leandro Prazeres Coelho <leandro.coelho@stz.com.br>
2022-04-26 21:20:38 -0300

    Exercicios

---
+++ LPA/exercicio1.py
@@ -0,0 +1,17 @@
+# Descubra a soma dos valores de 1-50 e impares
+soma = 0
+for i in range(1,51,2):
+    soma += i
+print('resultado de 1 + 50 impares é: ', soma)
+
+# Descubra a soma dos valores de 1-50 e pares
+soma = 0
+for i in range(0,51,2):
+    print(i)
+    soma += i
+print('resultado de 1 + 50 pares é: ', soma)
+
+
+
+
+
---
+++ LPA/exercicio2.py
@@ -0,0 +1,7 @@
+#LEIA O NOME E A QUANTIDADE DE VEZES QUE REPETE O NOME ESCRITO
+
+nome =input('Seu nome?')
+num = int(input('Quantas vezes?'))
+
+for i in range(num):
+    print(nome)
